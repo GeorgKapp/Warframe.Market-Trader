52d86fddab8c3c4876e20b32527d13f1d7139d90
Georg Kapp <georg.kapp@tis-cca.com>
2021-06-30 20:45:42 +0200

    Upgrade BasisSetup.sql

--- Warframe.Marker Infrastructure/BasisSetup.sql
+++ Warframe.Marker Infrastructure/BasisSetup.sql
@@ -9,6 +9,11 @@ CREATE SCHEMA [enum]
 Go
 -- Create Enum Tables
 
+create table [enum].[PlatformType] (
+	ID int not null primary key,
+    Type varchar(4) not null,
+);
+
 create table [enum].[IconFormatType] (
     ID int not null primary key,
     Type varchar(4) not null,
@@ -28,11 +33,50 @@ create table [enum].[StatusType] (
     ID int not null primary key,
     Type varchar(10) not null,
 );
+
+create table [enum].[RoleType] (
+	ID int not null primary key,
+    Type varchar(10) not null,
+);
+
 -- Create Content Tables
+
+create table [dbo].[LinkedAccounts] (
+    ID int not null identity(1,1) primary key,
+	HasSteamProfile bit not null,
+	HasPatreonProfile bit not null,
+	HasXboxProfile bit not null,
+	HasDiscordProfile bit not null
+);
 
 create table [dbo].[Tag] (
     ID int not null identity(1,1) primary key,
 	Text nvarchar(100),
+);
+
+create table [dbo].[User] (
+    ID int not null identity(1,1) primary key,
+	InGameName nvarchar(1000),
+	LastSeen datetimeoffset Null,
+	Reputation int not null,
+	Avatar nvarchar(max),
+	RegionID int foreign key references [enum].[RegionType](ID),
+	StatusID int foreign key references [enum].[StatusType](ID),
+);
+
+create table [dbo].[LoginUser] (
+    ID int not null identity(1,1) primary key,
+	CheckCode nvarchar(max),
+	Banned bit,
+	Anonymous bit,
+	HasMail bit,
+	Verification bit,
+	UnreadMessages tinyint,
+	WrittenReviews tinyint,
+	UserID int foreign key references [dbo].[User](ID),
+	PlatformID int foreign key references [enum].[PlatformType](ID),
+	RoleID int foreign key references [enum].[RoleType](ID),
+	LinkedAccountsID int foreign key references [dbo].[LinkedAccounts](ID),
 );
 
 create table [dbo].[Translation] (
@@ -65,6 +109,20 @@ create table [dbo].[Item] (
 	ItemNameID int foreign key references [dbo].[Translation](ID),
 );
 
+create table [dbo].[Order] (
+    ID int not null identity(1,1) primary key,
+	CreationDate DateTimeOffset not null,
+	LastUpdate DateTimeOffset not null,
+	Quantity int not null,
+	Visible bit not null,
+	Platinum int not null,
+	ModRank tinyint,
+	UserID int not null foreign key references [dbo].[User](ID),
+	PlatformTypeID int not null foreign key references [enum].[PlatformType](ID),
+	RegionTypeID int not null foreign key references [enum].[RegionType](ID),
+	OrderTypeID int not null foreign key references [enum].[OrderType](ID),
+);
+
 create table [dbo].[ItemTag] (
     ID int not null identity(1,1) primary key,
 	ItemID int not null foreign key references [dbo].[Item](ID),
@@ -80,6 +138,8 @@ create table [dbo].[SetItem] (
 
 
 -- Insert Enum Table Content
+
+insert into [enum].[PlatformType] (ID, Type) values (1, 'Pc')
 
 insert into [enum].[IconFormatType] (ID, Type) values (1, 'Land')
 insert into [enum].[IconFormatType] (ID, Type) values (2, 'Port')
@@ -97,5 +157,10 @@ insert into [enum].[RegionType] (ID, Type) values (6, 'Sv')
 insert into [enum].[StatusType] (ID, Type) values (1, 'Ingame')
 insert into [enum].[StatusType] (ID, Type) values (2, 'Offline')
 insert into [enum].[StatusType] (ID, Type) values (3, 'Online')
+
+insert into [enum].[RoleType] (ID, Type) values (1, 'Anonymous')
+insert into [enum].[RoleType] (ID, Type) values (2, 'User')
+insert into [enum].[RoleType] (ID, Type) values (3, 'Moderator')
+insert into [enum].[RoleType] (ID, Type) values (4, 'Admin')
 
 Go
